47583f8bc4988cadb587b0230399145339b4c2c8
kiboma <omayioben@gmail.com>
2023-07-27 08:27:49 +0300

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "categories#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories
 end
